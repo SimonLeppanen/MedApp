af297f5d62b147d39582a7cc003ce8eb2c73e6ab
Simon <simonleppanen@gmail.com>
2017-04-21 10:19:14 +0200

    Layout almost complete. Added PEF, sleep, :2

--- app/src/main/java/com/example/simon/medapp/PEFActivityEntry.java
+++ app/src/main/java/com/example/simon/medapp/PEFActivityEntry.java
@@ -3,15 +3,42 @@ package com.example.simon.medapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by simon on 2017-04-19.
  */
 
 public class PEFActivityEntry extends AppCompatActivity {
+
+    String day;
+    String time;
+    String value;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pef_entry);
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras == null) {
+            day = null;
+            time = null;
+            value = null;
+        } else {
+            day = extras.getString("DAY");
+            time = extras.getString("TIME");
+            value = extras.getString("VALUE");
+        }
+        TextView dayTextView = (TextView) findViewById(R.id.entry_day);
+        TextView timeTextView = (TextView) findViewById(R.id.entry_time);
+        TextView valueTextView = (TextView) findViewById(R.id.entry_value);
+
+        dayTextView.setText(day);
+        timeTextView.setText(time);
+        valueTextView.setText(value);
+
+
     }
 }
--- app/src/main/java/com/example/simon/medapp/PEFActivityMain.java
+++ app/src/main/java/com/example/simon/medapp/PEFActivityMain.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by simon on 2017-04-19.
@@ -19,6 +23,24 @@ public class PEFActivityMain extends AppCompatActivity {
 
     public void pefAddButton(View view) {
         Intent intent = new Intent(PEFActivityMain.this, PEFActivityAdd.class);
+        startActivity(intent);
+
+    }
+
+    public void pefEntryClicked(View view) {
+        TableRow row = (TableRow) view;
+        TextView textView1 = (TextView) row.getChildAt(0);
+        TextView textView2 = (TextView) row.getChildAt(1);
+        TextView textView3 = (TextView) row.getChildAt(2);
+
+        String day = textView1.getText().toString();
+        String time = textView2.getText().toString();
+        String value = textView3.getText().toString();
+
+        Intent intent = new Intent(PEFActivityMain.this, PEFActivityEntry.class);
+        intent.putExtra("DAY", day);
+        intent.putExtra("TIME", time);
+        intent.putExtra("VALUE", value);
         startActivity(intent);
 
     }
